d13b7570a4303bf42112df2cc87614a56f8e1248
Emmanuellensossani <emmanuelle.nsossani@etu.u-pec.fr>
2023-05-22 16:50:28 +0200

    curseur

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const cursor = document.querySelector('.cursor');
+const cursorWidth = cursor.offsetWidth;
+const cursorHeight = cursor.offsetHeight;
 
-document.addEventListener('mousemove', e=> {
-    cursor.setAttribute('style','top'+(e.pageY)+"px: left:"+
-    (e.page)+"px")
-})
+document.addEventListener("mousemove", (e) => {
+  const x = e.clientX - cursorWidth / 2;
+  const y = e.clientY - cursorHeight / 2;
+
+  cursor.style.top = `${y}px`;
+  cursor.style.left = `${x}px`;
+});
 
