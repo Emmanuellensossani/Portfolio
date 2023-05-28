0c93b861055438ef6103fb29b8526b08ee81d847
Emmanuellensossani <emmanuelle.nsossani@etu.u-pec.fr>
2023-05-29 01:50:36 +0200

    landing

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
-const cursor = document.querySelector('.cursor');
-const cursorWidth = cursor.offsetWidth;
-const cursorHeight = cursor.offsetHeight;
+function MouseWelcome() {
+  const cursor = document.querySelector('.cursor');
+  const cursorWidth = cursor.offsetWidth;
+  const cursorHeight = cursor.offsetHeight;
 
-document.addEventListener("mousemove", (e) => {
-  const x = e.clientX - cursorWidth / 2;
-  const y = e.clientY - cursorHeight / 2;
+  document.getElementById("welcome").addEventListener("mouseover", (e) => {
+    document.addEventListener("mousemove", onMouseMove);
+  });
 
-  cursor.style.top = `${y}px`;
-  cursor.style.left = `${x}px`;
-});
+  document.getElementById("welcome").addEventListener("mouseout", (e) => {
+    document.removeEventListener("mousemove", onMouseMove);
+  });
+
+  function onMouseMove(e) {
+    const x = e.clientX + window.pageXOffset - cursorWidth / 2;
+    const y = e.clientY + window.pageYOffset - cursorHeight / 2;
+
+    cursor.style.top = `${y}px`;
+    cursor.style.left = `${x}px`;
+  }
+}
+
+document.addEventListener("mousemove",parallax);
+function parallax(a){
+  document.querySelectorAll(".square").forEach(function(move){
+    var moving_value = move.getAttribute("data-value");
+    var x = (a.clientX*moving_value) / 250;
+    var y =(a.clientY * moving_value) / 250;
+
+    move.style.transform = "translateX("+x +"px) translateY("+y+"px)";
+
+  });
+}
